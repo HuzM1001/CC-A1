cdaebedf703dc9ea35f957746eb8f054f97f23f3
HuzaM1001 <brooklynmasefield773@gmail.com>
2026-02-14 18:35:31 +0500

    basic classes - first commit

---
+++ Token.java
@@ -0,0 +1,18 @@
+public class Token {
+    public TokenType type;
+    public String lexeme;
+    public int line;
+    public int column;
+
+    public Token(TokenType type, String lexeme, int line, int column) {
+        this.type = type;
+        this.lexeme = lexeme;
+        this.line = line;
+        this.column = column;
+    }
+
+    @Override
+    public String toString() {
+        return "<" + type + ", \"" + lexeme + "\", Line: " + line + ", Col: " + column + ">";
+    }
+}
